00912478b67500fa95f20e1ec9cad7736607fb37
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-01-18 20:18:07 +0530

    update

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// your code here
+document.getElementById("button").addEventListener("click", function (event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var year = document.getElementById("year").value;
+    var url;
+    if (name != "" && year != "") {
+        url = "https://localhost:8080/?" + "name=" + name + "&year=" + year;
+    } else if (name != "") {
+        url = "https://localhost:8080/?" + "name=" + name;
+    } else if (year != "") {
+        url = "https://localhost:8080/?" + "year=" + year;
+    } else {
+        url = "https://localhost:8080/";
+    }
+    document.getElementById("url").innerHTML = url;
+});
